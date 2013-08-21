899e5c61343a48e237afb9f5b92563312e83c028
erodrigueza <rodriguez.arreola@gmail.com>
2013-08-20 19:24:50 -0500

    add control transaction

--- Interface_SAP-QS3/src/main/java/com/danone/bonafont/batch/reader/FlatFileReader.java
+++ Interface_SAP-QS3/src/main/java/com/danone/bonafont/batch/reader/FlatFileReader.java
@@ -5,8 +5,14 @@ package com.danone.bonafont.batch.reader;
 
 import java.io.File;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
 
 import org.apache.log4j.Logger;
+import org.springframework.batch.item.ExecutionContext;
+import org.springframework.batch.item.ItemStreamException;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.core.io.Resource;
 
@@ -20,12 +26,27 @@ public class FlatFileReader extends FlatFileItemReader<OrdenInputDTO> {
 	private static final Logger LOG = Logger.getLogger(FlatFileReader.class);
 	private Resource resource;
 	private boolean isError = false;
+	private Integer idArchivo;
+	private DataSource datasource;
 
 	@Override
 	public void setResource(Resource resource) {
 		LOG.debug("File Name: " + resource.getFilename());
 		this.resource = resource;
+		this.idArchivo = getIdArchivo(resource);
 		super.setResource(resource);
+	}
+
+	private Integer getIdArchivo(Resource resource) {
+		try {
+			String nameFile = resource.getFilename();
+			Connection conn = datasource.getConnection();
+			String query = "";
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
 	}
 
 	@Override
@@ -36,12 +57,17 @@ public class FlatFileReader extends FlatFileItemReader<OrdenInputDTO> {
 
 	@Override
 	protected OrdenInputDTO doRead() throws Exception {
+		OrdenInputDTO dto = null;
 		try {
-			return super.doRead();
+			dto = super.doRead();
 		} catch (Exception e) {
 			this.isError = true;
 			throw e;
 		}
+		if (dto != null) {
+			dto.setNu_id_archivo(2);
+		}
+		return dto;
 	}
 
 	private void moveFile() throws IOException {
@@ -58,4 +84,20 @@ public class FlatFileReader extends FlatFileItemReader<OrdenInputDTO> {
 		}
 	}
 	
+	@Override
+	public void update(ExecutionContext executionContext)
+			throws ItemStreamException {
+		if(executionContext.get("anErrorHappened")!= null){
+			this.isError = executionContext.getInt("anErrorHappened") == 1; 
+		}	
+		super.update(executionContext);
+	}
+
+	public DataSource getDatasource() {
+		return datasource;
+	}
+
+	public void setDatasource(DataSource datasource) {
+		this.datasource = datasource;
+	}
 }
