bd98e3fba0f7076b1f5ef43b89963db9475cb755
erodrigueza <rodriguez.arreola@gmail.com>
2013-10-28 13:39:37 -0600

    Se modifica para que solo se lance una vez la exception al tener registros duplicados

--- Interface_SAP-QS3/src/main/java/com/danone/bonafont/batch/writer/JdbcWriterSapOrden.java
+++ Interface_SAP-QS3/src/main/java/com/danone/bonafont/batch/writer/JdbcWriterSapOrden.java
@@ -7,7 +7,6 @@ import javax.annotation.Resource;
 import org.apache.log4j.Logger;
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
 
-import com.danone.bonafont.batch.dao.ArchivoDAO;
 import com.danone.bonafont.batch.dao.SapOrdenDAO;
 import com.danone.bonafont.batch.model.SapOrden;
 import com.danone.bonafont.batch.util.Constants;
@@ -20,27 +19,23 @@ public class JdbcWriterSapOrden extends JdbcBatchItemWriter<SapOrden> {
 
 	private static final Logger LOG = Logger.getLogger(JdbcWriterSapOrden.class);
 
-	protected boolean isDuplicate;
-	protected Long idArchivo;
+	protected boolean threwException;
 
 	@Resource
 	SapOrdenDAO sapOrdenDAO;
 
-	@Resource
-	ArchivoDAO archivoDAO;
-
 	@Override
 	public void write(List<? extends SapOrden> items) throws Exception {
-		isDuplicate = false;
 		for (SapOrden orden : items) {
 			LOG.info("ch_foliopedido: " + orden.getCh_foliopedido());
 			LOG.info("ch_producto: " + orden.getCh_producto());
 			List<SapOrden> ordens = sapOrdenDAO.findByFolioProducto(orden);
 			if (ordens.size() > 0) {
 				orden.setNu_id_estatus(Constants.REG_DUPLICADO);
-				isDuplicate = true;
-				idArchivo = orden.getNu_id_archivo();
-				throw new Exception(Constants.ERR_DATA_DUPL);
+				if(!threwException){
+					threwException = true;
+					throw new Exception(Constants.ERR_DATA_DUPL);
+				}
 			} else {
 				orden.setNu_id_estatus(Constants.REG_NUEVO);
 			}
--- Interface_SAP-QS3/src/main/java/com/danone/bonafont/batch/writer/JdbcWriterSapOrdenCan.java
+++ Interface_SAP-QS3/src/main/java/com/danone/bonafont/batch/writer/JdbcWriterSapOrdenCan.java
@@ -7,9 +7,7 @@ import javax.annotation.Resource;
 import org.apache.log4j.Logger;
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
 
-import com.danone.bonafont.batch.dao.ArchivoDAO;
 import com.danone.bonafont.batch.dao.SapOrdenCanDAO;
-import com.danone.bonafont.batch.model.Archivo;
 import com.danone.bonafont.batch.model.SapOrdenCan;
 import com.danone.bonafont.batch.util.Constants;
 
@@ -20,41 +18,29 @@ import com.danone.bonafont.batch.util.Constants;
 public class JdbcWriterSapOrdenCan extends JdbcBatchItemWriter<SapOrdenCan> {
 
 	private static final Logger LOG = Logger.getLogger(JdbcWriterSapOrden.class);
-	protected boolean isDuplicate;
-	protected Long idArchivo;
-
+	
+	protected boolean threwException;
+	
 	@Resource
 	SapOrdenCanDAO sapOrdenCanDAO;
 
-	@Resource
-	ArchivoDAO archivoDAO;
-
 	@Override
 	public void write(List<? extends SapOrdenCan> items) throws Exception {
-		isDuplicate = false;
 		for (SapOrdenCan orden : items) {
 			LOG.info("ch_foliopedido: " + orden.getCh_foliopedido());
 			LOG.info("ch_producto: " + orden.getCh_producto());
 			List<SapOrdenCan> ordens = sapOrdenCanDAO.findByFolioProducto(orden);
 			if (ordens.size() > 0) {
 				orden.setNu_id_estatus(Constants.REG_DUPLICADO);
-				isDuplicate = true;
-				idArchivo = orden.getNu_id_archivo();
+				if(!threwException){
+					threwException = true;
+					throw new Exception(Constants.ERR_DATA_DUPL);
+				}
 			} else {
 				orden.setNu_id_estatus(Constants.REG_NUEVO);
 			}
 		}
-		updateFile();
 		super.write(items);
-	}
-
-	private void updateFile() {
-		if (idArchivo != null && isDuplicate) {
-			Archivo archivo = archivoDAO.findByPK(Archivo.class, idArchivo);
-			archivo.setNu_id_estatus(Constants.ARCHIVO_ERROR);
-			archivo.setCh_descripcion("Archivo con registros duplicados");
-			archivoDAO.update(archivo);
-		}
 	}
 
 }
